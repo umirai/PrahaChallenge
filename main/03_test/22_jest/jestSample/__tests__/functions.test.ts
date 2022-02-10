8954e0e3c7eb2f9ee106a34836b2d8e32dcb7f2e
UmedaMirai <umdmira128@gmail.com>
2022-02-11 08:52:54 +0900

    feat: getFirstNameThrowIfLongのテストを追加

--- main/03_test/22_jest/jestSample/__tests__/functions.test.ts
+++ main/03_test/22_jest/jestSample/__tests__/functions.test.ts
@@ -1,11 +1,19 @@
+import axios from "axios";
+
 // functions
 import { sumOfArray } from "../functions";
 import { asyncSumOfArray } from "../functions";
 import { asyncSumOfArraySometimesZero } from "../functions";
+import { getFirstNameThrowIfLong } from "../functions";
+import { NameApiService } from "../nameApiService";
 import * as DatabaseMockService from "../util";
 
 // services
 import { DatabaseMock } from "../util";
+
+// mock
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 // --------------------------------------------------
 
@@ -79,6 +87,38 @@ describe("asyncSumOfArraySometimesZero", (): void => {
       const database = new DatabaseMock();
       const res = await asyncSumOfArraySometimesZero(arr, database);
       expect(res).toBe(0);
+    });
+  });
+});
+
+// getFirstNameThrowIfLong
+describe("getFirstNameThrowIfLong", (): void => {
+  beforeAll((): void => {
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    const params = { data: { first_name: "mira" } }
+    mockedAxios.get.mockResolvedValue(params);
+  });
+
+  afterAll((): void => {
+    jest.clearAllMocks();
+  });
+
+  describe("正常系", (): void => {
+    it("default", async (): Promise<void> => {
+      const maxLength = 5;
+      const nameApiService = new NameApiService();
+      const res = await getFirstNameThrowIfLong(maxLength, nameApiService);
+      expect(res).toBe("mira");
+    });
+  });
+
+  describe("異常系", (): void => {
+    it("default", async (): Promise<void> => {
+      const maxLength = 3;
+      const nameApiService = new NameApiService();
+      await expect(
+        getFirstNameThrowIfLong(maxLength, nameApiService)
+      ).rejects.toThrow();
     });
   });
 });
