58965f0b61e496477eb2ecd98f6796f005c01656
UmedaMirai <umdmira128@gmail.com>
2022-02-10 14:51:43 +0900

    feat: sumOfArrayのテストを追加

--- main/03_test/22_jest/jestSample/__tests__/functions.test.ts
+++ main/03_test/22_jest/jestSample/__tests__/functions.test.ts
@@ -1,1 +1,34 @@
-// todo: ここに単体テストを書いてみましょう！
+// functions
+import { sumOfArray } from "../functions";
+
+// sumOfArray
+describe("sumOfArray", (): void => {
+  describe("正常系", (): void => {
+    it("default", (): void => {
+      const arr: number[] = [1, 2, 3];
+      expect(sumOfArray(arr)).toBe(6);
+    });
+
+    it("include zero", (): void => {
+      const arr: number[] = [0, 2, 3];
+      expect(sumOfArray(arr)).toBe(5);
+    });
+
+    it("include nevative number", (): void => {
+      const arr: number[] = [1, 2, -3];
+      expect(sumOfArray(arr)).toBe(0);
+    });
+
+    it("include float number", (): void => {
+      const arr: number[] = [1, 2, 3.5];
+      expect(sumOfArray(arr)).toBeCloseTo(6.5);
+    });
+  });
+
+  describe("異常系", (): void => {
+    it("empty", (): void => {
+      const arr: number[] = [];
+      expect((): number => sumOfArray(arr)).toThrow();
+    });
+  });
+});
