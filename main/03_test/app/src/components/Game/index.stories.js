592a68d8fe58c9f28fc3921c5adc67c627d47e3b
UmedaMirai <umdmira128@gmail.com>
2022-02-17 11:58:24 +0900

    test: Gameコンポーネントに引き分けのstoryを追加

--- main/03_test/app/src/components/Game/index.stories.js
+++ main/03_test/app/src/components/Game/index.stories.js
@@ -10,3 +10,11 @@ export default {
 const Template = (args) => <Game {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  history: [{ squares: Array(9).fill(null) }]
+}
+
+export const Draw = Template.bind({});
+Draw.args = {
+  history: [{ squares: ["X", "O", "X", "O", "X", "X", "O", "X", "O"] }]
+}
