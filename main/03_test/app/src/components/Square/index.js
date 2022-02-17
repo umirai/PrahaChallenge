4b35c501b705bbb1f0b6fcf97e23adfd3ccbef3c
UmedaMirai <umdmira128@gmail.com>
2022-02-17 10:26:07 +0900

    test: Squareコンポーネントを識別するため、カスタム属性を追加

--- main/03_test/app/src/components/Square/index.js
+++ main/03_test/app/src/components/Square/index.js
@@ -1,6 +1,6 @@
 export const Square = (props) => {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick} data-square={`square_${props.number}`}>
       {props.value}
     </button>
   );
